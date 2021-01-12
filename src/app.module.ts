a6a5b51242ebb2e5521512377bc4319ddfb1c067
cit007 <itcho0077@gmail.com>
2021-01-12 23:27:58 +0900

    auth module and guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Podcast } from './podcast/entities/podcast.entity';
 import { Episode } from './podcast/entities/episode.entity';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UsersModule } from './users/users.module';
     GraphQLModule.forRoot({ autoSchemaFile: true }),
     PodcastsModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
