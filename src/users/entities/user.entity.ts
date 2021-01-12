93ca3a924e8c8629898707935d13064a5e3038e8
cit007 <itcho0077@gmail.com>
2021-01-12 23:25:25 +0900

    user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,80 @@
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
+
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
+import { IsEmail, IsEnum } from 'class-validator';
+
+enum UserRole {
+  Host,
+  Listener,
+}
+
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@InputType({ isAbstract: true })
+@ObjectType()
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
+  @Column()
+  @Field(type => String)
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @Field(type => String)
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole })
+  @Field(type => UserRole)
+  @IsEnum(UserRole)
+  role: UserRole;
+
+  @Column({ default: false })
+  @Field(type => Boolean)
+  verified: boolean;
+
+  @CreateDateColumn()
+  @Field(type => Date)
+  creeatedAt: Date;
+
+  @CreateDateColumn()
+  @Field(type => Date)
+  updatedAt: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    try {
+      console.log('password hash!!!!');
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async checkPassword(pwd: string): Promise<boolean> {
+    try {
+      const ok = await bcrypt.compare(pwd, this.password);
+      return ok;
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
